80fedb2cf5c5b5ddf47e75e7c75892f4f5287599
Xch1998h <xch.harutyunyan1998@mail.ru>
2018-12-25 07:20:48 +0300

    Khachik Harutyunyan

---
+++ src/components/FooterDown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+let FooterDown =()=>{
+    return(
+        <div>
+            <p className='foo'> <span>©</span> 2018 |  by <b>Khachik Harutyunyan</b> | <span>Thank you for visiting</span></p>
+        </div>
+    );
+}
+export default FooterDown;
